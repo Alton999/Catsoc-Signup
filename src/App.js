a32c9f8a6c569efefb0f9da5262410b75e60e703
Alton <alton_ong@live.com.sg>
2021-07-25 00:51:26 +1000

    Added new API endpoint and integration with handle POST

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+	const [data, setData] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+		USU: ""
+	});
+	const { firstName, lastName, email, USU } = data;
+	const handleChange = (e) => {
+		setData({
+			...data,
+			[e.target.name]: e.target.value
+		});
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await fetch(
+				"https://v1.nocodeapi.com/altonong/google_sheets/bXWiEQmJJndwtgGS?tabId=Sheet1",
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify([
+						[firstName, lastName, email, USU, new Date().toLocaleString()]
+					])
+				}
+			);
+			await response.json();
+			setData({ ...data, firstName: "", lastName: "", email: "", usu: "" });
+		} catch (err) {
+			console.log(err);
+		}
+	};
 	return <div>This is the home page for the App</div>;
 };
 
